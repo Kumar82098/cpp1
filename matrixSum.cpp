d6ecfd546c0d7fff8633c29058c54828aca9a0fb
Kumar82098 <56931055+Kumar82098@users.noreply.github.com>
2019-10-24 02:51:44 +0530

    Update matrixSum.cpp

--- matrixSum.cpp
+++ matrixSum.cpp
@@ -4,14 +4,14 @@ using namespace std;
 int main()
 {
   int a,b,i,j,first[10][10],second[10][10],sum[10][10];
-  cout<<"Enter the no. of row and column of 1st matrix: "<<endl;
+  cout<<"Enter the number of row and column of 1st matrix: "<<endl;
   cin>>a>>b;
   cout<<"Enter elements: "<<endl;
   for(i=0;i<a;i++)
    for(j=0;j<b;j++)
      cin>>first[i][j];
   
-  cout<<"Enter the no. of row and column of 2nd matrix: "<<endl;
+  cout<<"Enter the number of row and column of 2nd matrix: "<<endl;
   cin>>a>>b;
   
   cout<<"Enter elements: "<<endl;
